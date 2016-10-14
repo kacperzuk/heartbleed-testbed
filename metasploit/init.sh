a91ceae03edd6e2267d6b17a3e269a5defd613f5
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-10-14 23:20:05 +0200

    Working metasploit

--- metasploit/init.sh
+++ metasploit/init.sh
@@ -4,4 +4,9 @@ source /usr/local/rvm/scripts/rvm
 /etc/init.d/postgresql start
 /opt/msf/msfupdate --git-branch master
 cd /tmp/data
-msfconsole -r /hb.rc
+echo "Running explooit:"
+cat /hb.rc
+bash -c "cd /tmp/data && GEM_HOME=/usr/local/rvm/gems/ruby-2.3.1@metasploit-framework msfconsole -r /hb.rc"
+echo "Done."
+bash -c "cd /tmp/data && bash"
+
