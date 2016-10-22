eeb66fd0dbeda3fa94babe1cebbe91162eddb19f
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-10-22 17:40:22 +0200

    Debug docker autobuild

--- metasploit/init.sh
+++ metasploit/init.sh
@@ -3,5 +3,5 @@
 source /usr/local/rvm/scripts/rvm
 /etc/init.d/postgresql start
 cd /tmp/data
-bash -c "cd /tmp/data && GEM_HOME=/usr/local/rvm/gems/ruby-2.3.1@metasploit-framework msfconsole -r /$1"
+bash -c "cd /tmp/data && GEM_HOME=/usr/local/rvm/gems/ruby-2.3.1@metasploit-framework msfconsole -r /scripts/$1"
 echo "Done."
